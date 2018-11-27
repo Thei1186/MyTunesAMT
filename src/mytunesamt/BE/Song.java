a585e3e9a66899c62001665b00dbc3b2438a8666
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2018-11-27 11:52:06 +0100

    Update Song.java

--- src/mytunesamt/BE/Song.java
+++ src/mytunesamt/BE/Song.java
@@ -12,4 +12,36 @@ package mytunesamt.BE;
 public class Song
 {
     private String title;
+    private String artist;
+    private String category;
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public void setArtist(String artist)
+    {
+        this.artist = artist;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
 }
