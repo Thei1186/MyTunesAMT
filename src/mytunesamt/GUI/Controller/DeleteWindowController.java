eeaedadff868f1cc9950d5d8b1c56e86df9a85ff
Asvør Rasmussen <asvo0011@easv365.dk>
2018-11-28 09:38:07 +0100

    gyk

--- src/mytunesamt/GUI/Controller/DeleteWindowController.java
+++ src/mytunesamt/GUI/Controller/DeleteWindowController.java
@@ -9,7 +9,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -20,7 +20,7 @@ public class DeleteWindowController implements Initializable
 {
 
     @FXML
-    private AnchorPane btnDeleteFinal;
+    private Button btnDeleteFinal;
 
     /**
      * Initializes the controller class.
