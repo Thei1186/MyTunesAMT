9902fb44aef81433193a43d33d2fd4acc908bb78
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2018-12-01 09:59:41 +0100

    DbDao, manager og tilhørende model

--- src/mytunesamt/BLL/TunesManager.java
+++ src/mytunesamt/BLL/TunesManager.java
@@ -5,6 +5,8 @@
  */
 package mytunesamt.BLL;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import mytunesamt.BE.Song;
 import mytunesamt.DAL.Database.PlaylistDbDao;
@@ -14,15 +16,45 @@ import mytunesamt.DAL.Database.SongDbDao;
  *
  * @author kokus
  */
-public class TunesManager
+public class TunesManager implements MTLogicFacade
 {
 
-    PlaylistDbDao pLDbDao = new PlaylistDbDao();
-//    SongDbDao sDbDao = new SongDbDao();
+    PlaylistDbDao pLDbDao;
+    SongDbDao sDbDao;
 
-//public List<Song> getAllSongs()
-//{
-// return sDbDao;   
-//}
+    public TunesManager() throws IOException
+    {
+        this.sDbDao = new SongDbDao();
+        this.pLDbDao = new PlaylistDbDao();
+    }
 
+    @Override
+    public List<Song> getAllSongs() throws SQLException
+    {
+        return sDbDao.getAllSongs();
+    }
+
+    @Override
+    public Song getSong(Song song)
+    {
+        return sDbDao.getSong(song.getId());
+    }
+
+    @Override
+    public void addSong(Song song) throws SQLException
+    {
+        sDbDao.addSong(song);
+    }
+
+    @Override
+    public void editSong(Song song)
+    {
+        sDbDao.editSong(song);
+    }
+
+    @Override
+    public void deleteSong(Song song)
+    {
+        sDbDao.deleteSong(song);
+    }
 }
